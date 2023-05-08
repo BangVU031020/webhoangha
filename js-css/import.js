da510d21ef817828e7bf7d990b71e4d848d9d684
bangvu <bangv667@gmail.com>
2023-05-08 21:42:21 +0700

    first commit

---
+++ js-css/import.js
@@ -0,0 +1,7 @@
+$(function () {
+    $("#top-navigation").load("./common/top-navigation.html");
+    $("#heading").load("./common/heading.html");
+    $("#nav").load("./common/nav.html");
+    $("#slider").load("./common/slider.html");
+    $("#footer").load("./common/footer.html");
+});
